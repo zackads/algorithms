005d799299c444d6c06379d8ea4b3d85fc139ea2
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-12-14 20:08:07 +0000

    Implement bubble sort

--- src/main/java/io/zackads/github/BubbleSort.java
+++ src/main/java/io/zackads/github/BubbleSort.java
@@ -2,6 +2,21 @@ package io.zackads.github;
 
 public class BubbleSort {
     static int[] sort(int[] array) {
-        return null;
+
+        boolean swapped = false;
+        do {
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                    swapped = true;
+                } else {
+                    swapped = false;
+                }
+            }
+        } while (swapped);
+
+        return array;
     }
 }
--- src/test/java/io/zackads/github/BubbleSortTest.java
+++ src/test/java/io/zackads/github/BubbleSortTest.java
@@ -1,10 +1,39 @@
 package io.zackads.github;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
-public class BubbleSortTest extends TestCase {
+import static org.junit.Assert.assertArrayEquals;
 
+public class BubbleSortTest {
+
+    @Test
     public void givenAnEmptyArray__ThenReturnEmptyArray() {
+        int[] input = new int[0];
+        int[] expectedOutput = new int[0];
 
+        int[] output = BubbleSort.sort(input);
+
+        assertArrayEquals(output, expectedOutput);
     }
+
+    @Test
+    public void givenArrayOfOneElement__ThenReturnArray() {
+        int[] input = {0};
+        int[] expectedOutput = {0};
+
+        int[] output = BubbleSort.sort(input);
+
+        assertArrayEquals(output, expectedOutput);
+    }
+
+    @Test
+    public void givenArrayOfTwoElementsOutOfOrder__ThenReturnArrayInOrder() {
+        int[] input = {1, 0};
+        int[] expectedOutput = {0, 1};
+
+        int[] output = BubbleSort.sort(input);
+
+        assertArrayEquals(output, expectedOutput);
+    }
+
 }
