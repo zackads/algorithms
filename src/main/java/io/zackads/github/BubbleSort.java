85931e72ecf192f991e8a69561ef3dccdcd86406
Zack Adlington <54271433+zackads@users.noreply.github.com>
2021-12-14 19:44:58 +0000

    Initial commit

---
+++ src/main/java/io/zackads/github/BubbleSort.java
@@ -0,0 +1,7 @@
+package io.zackads.github;
+
+public class BubbleSort {
+    static int[] sort(int[] array) {
+        return null;
+    }
+}
---
+++ src/test/java/io/zackads/github/BubbleSortTest.java
@@ -0,0 +1,10 @@
+package io.zackads.github;
+
+import junit.framework.TestCase;
+
+public class BubbleSortTest extends TestCase {
+
+    public void givenAnEmptyArray__ThenReturnEmptyArray() {
+
+    }
+}
